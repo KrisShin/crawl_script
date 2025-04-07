3fee74b0d9265b43d00538b026eb07ee940c83ce
Kris <KrisShin5014@outlook.com>
2025-04-07 17:12:41 +0800

    remove unvalid field description.

--- crawl_script/models/base_model.py
+++ crawl_script/models/base_model.py
@@ -3,9 +3,9 @@ from tortoise import fields, models
 
 class BaseModel(models.Model):
     # id = fields.CharField(pk=True, max_length=32, default=generate_random_id)
-    id = fields.BigIntField(primary_key=True, description='唯一主键')
-    create_time = fields.DatetimeField(auto_now_add=True, description='创建时间')
-    update_time = fields.DatetimeField(auto_now=True, description='更新时间')
+    id = fields.BigIntField(primary_key=True)
+    create_time = fields.DatetimeField(auto_now_add=True)
+    update_time = fields.DatetimeField(auto_now=True)
 
     class Meta:
         abstract = True
