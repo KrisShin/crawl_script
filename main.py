e3de3065274513c3d48200bad4b5bd6bd89f567a
Kris <KrisShin5014@outlook.com>
2025-04-18 16:59:12 +0800

    update args.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import click
 from loguru import logger
 from common.global_variant import init_db
-from app.xueqiu.main import crawl_index, crawl_zh, crawl_zh_async, crawl_zh_history_async, crawl_user_async
+from app.xueqiu.main import crawl_index, crawl_rebalancing_async, crawl_zh, crawl_zh_async, crawl_zh_history_async, crawl_user_async
 
 
 def connect_db(db_type: str):
@@ -13,7 +13,7 @@ def connect_db(db_type: str):
 
 
 @click.command()
-@click.argument('spider_name', type=click.Choice(['index', 'zh_single', 'zh', 'zh_his', 'user'], case_sensitive=False))
+@click.argument('spider_name', type=click.Choice(['index', 'zh_single', 'zh', 'zh_his', 'user', 'reb'], case_sensitive=False))
 @click.argument('start_id', type=int)
 @click.argument('end_id', type=int)
 @click.argument('coroutine_count', type=int)
@@ -38,16 +38,18 @@ def run_spider(spider_name, start_id: int, end_id: int, coroutine_count: int):
         asyncio.run(crawl_zh_history_async(start_id, end_id, coroutine_count))
     elif spider_name == 'user':
         asyncio.run(crawl_user_async(start_id, end_id, coroutine_count))
+    elif spider_name == 'reb':
+        asyncio.run(crawl_rebalancing_async(start_id, end_id, coroutine_count))
     else:
         logger.error(f"未知的爬虫名称: {spider_name}")
 
 
 if __name__ == '__main__':
-    # run_spider()
-    import json
+    run_spider()
+    # import json
 
-    with open('./user_id.json', 'r') as f:
-        data = json.load(f)
-    with open('./user_id.json', 'w') as f:
-        f.write(json.dumps([item['owner_id'] for item in data]))
-    print(len(data))
+    # with open('./user_id.json', 'r') as f:
+    #     data = json.load(f)
+    # with open('./user_id.json', 'w') as f:
+    #     f.write(json.dumps([item['owner_id'] for item in data]))
+    # print(len(data))
