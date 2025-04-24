0105ac69dd5e04b51b00e40e33154f78cd928b84
Kris <KrisShin5014@outlook.com>
2025-04-24 16:30:44 +0800

    analyze.

--- app/xueqiu/analyze.py
+++ app/xueqiu/analyze.py
@@ -3,13 +3,13 @@ import json
 from loguru import logger
 from app.xueqiu.model import XueqiuZHHistory
 
-BATCH_SIZE = 1000
+BATCH_SIZE = 100
 
 
 async def get_good_zh():
 
-    symbols = []
     last_id = 0
+    symbols = []
     while True:
         batch = await XueqiuZHHistory.filter(id__gt=last_id).order_by('id').limit(BATCH_SIZE)
 
@@ -35,12 +35,12 @@ async def get_good_zh():
                 if min_value >= 1:
                     symbols.append(record.symbol)
                 logger.info(
-                    f"组合:{record.name}-{record.symbol} 的历史数据长度: {his_len}, 最小净值: {min_value}, 符合条件:{'√' if min_value >= 1 and his_len>365*3 else '×'}"
+                    f"组合:{record.name}-{record.symbol} 的历史数据长度: {his_len}, 最小净值: {min_value}, 符合条件:{'👌🏻' if min_value >= 1 and his_len>365*3 else '🚫'}"
                 )
 
             except Exception as e:
                 print(f"Parse failed for {record.symbol}: {e}")
-    with open("good_zh.json", "w") as f:
-        f.write(json.dumps(symbols, indent=4, ensure_ascii=False))
-        logger.success(f"成功写入 {len(symbols)} 条数据到 good_zh.json")
+        with open("good_zh.json", "w") as f:
+            f.write(json.dumps(symbols, indent=4, ensure_ascii=False))
+            logger.success(f"成功写入 {len(symbols)} 条数据到 good_zh.json")
     return symbols
